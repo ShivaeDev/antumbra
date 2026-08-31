8f0d5a97b7f991a93c57fa43691e6f35bb5ec239
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-08-31 04:31:14 +0100

    Name change link effects

--- packages/changes/src/change-submissions/links.ts
+++ packages/changes/src/change-submissions/links.ts
@@ -2,27 +2,25 @@ import { Database } from "@antumbra/persistence";
 import { Effect, Option } from "effect";
 import { changeRow } from "#change-read.ts";
 
-export const activeChange = (key: string) =>
-	Effect.gen(function* () {
-		const db = yield* Database;
-		const stored = yield* db.Change.where({ submissionKey: key }).first();
-		return yield* Option.match(stored, {
-			onNone: () => Effect.succeed(Option.none()),
-			onSome: (row) => Effect.map(changeRow(row), Option.some),
-		});
+export const activeChange = Effect.fn("changes.activeChange")(function* (key: string) {
+	const db = yield* Database;
+	const stored = yield* db.Change.where({ submissionKey: key }).first();
+	return yield* Option.match(stored, {
+		onNone: () => Effect.succeed(Option.none()),
+		onSome: (row) => Effect.map(changeRow(row), Option.some),
 	});
+});
 
-export const linkProduces = (pieceId: string, changeId: string) =>
-	Effect.gen(function* () {
-		const db = yield* Database;
-		const linked = yield* db.PieceChange.where({ changeId, pieceId }).first();
-		if (Option.isSome(linked)) {
-			return false;
-		}
-		yield* db.PieceChange.create({
-			changeId,
-			pieceId,
-			purpose: "produces",
-		});
-		return true;
+export const linkProduces = Effect.fn("changes.linkProduces")(function* (pieceId: string, changeId: string) {
+	const db = yield* Database;
+	const linked = yield* db.PieceChange.where({ changeId, pieceId }).first();
+	if (Option.isSome(linked)) {
+		return false;
+	}
+	yield* db.PieceChange.create({
+		changeId,
+		pieceId,
+		purpose: "produces",
 	});
+	return true;
+});

20732347532ba2f18c0113c565a82ac649a53c0c
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-08-31 09:36:32 +0100

    Inline repository graph deletion

--- packages/repos/src/forget.ts
+++ packages/repos/src/forget.ts
@@ -2,25 +2,20 @@ import { DomainFeeds } from "@antumbra/domain-feeds";
 import { Database } from "@antumbra/persistence";
 import { Effect } from "effect";
 
-const deleteRepoGraph = (id: string) =>
-	Effect.gen(function* () {
-		const db = yield* Database;
-		const changes = yield* db.Change.where({ repoId: id }).all();
-		yield* Effect.forEach(
-			changes,
-			(change) =>
-				db.ChangeTransition.where({ changeId: change.id })
-					.deleteAll()
-					.pipe(Effect.andThen(db.PieceChange.where({ changeId: change.id }).deleteAll())),
-			{ discard: true },
-		);
-		yield* db.Change.where({ repoId: id }).deleteAll();
-		yield* db.Repo.where({ id }).deleteAll();
-	});
-
 export const forgetRepo = Effect.fn("repos.forget")(function* (id: string) {
+	const db = yield* Database;
 	const feeds = yield* DomainFeeds;
-	yield* deleteRepoGraph(id);
+	const changes = yield* db.Change.where({ repoId: id }).all();
+	yield* Effect.forEach(
+		changes,
+		(change) =>
+			db.ChangeTransition.where({ changeId: change.id })
+				.deleteAll()
+				.pipe(Effect.andThen(db.PieceChange.where({ changeId: change.id }).deleteAll())),
+		{ discard: true },
+	);
+	yield* db.Change.where({ repoId: id }).deleteAll();
+	yield* db.Repo.where({ id }).deleteAll();
 	yield* feeds.publishFleetRefresh();
 	yield* feeds.publishVoyageRefresh();
 });

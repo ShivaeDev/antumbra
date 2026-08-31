26ddefca60294e5b5b032da930f2a6fcd998b2de
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-08-31 19:46:54 +0000

    Keep voyage feeds inside Effect services (#579)

--- packages/domain/src/voyage-feed.ts
+++ packages/domain/src/voyage-feed.ts
@@ -1,17 +1,16 @@
 import type { SightFailure } from "@antumbra/contract";
-import { DomainFeeds, type DomainFeedsService } from "@antumbra/domain-feeds";
+import { DomainFeeds } from "@antumbra/domain-feeds";
 import { Effect, Stream } from "effect";
-
-export const voyageWorldTicks = (feeds: DomainFeedsService) =>
-	Effect.gen(function* () {
-		const voyageWrites = yield* feeds.subscribeVoyageRefresh();
-		const fleetWrites = yield* feeds.subscribeFleetRefresh();
-		return Stream.merge(Stream.fromSubscription(voyageWrites), Stream.fromSubscription(fleetWrites));
-	});
 
 export const makeVoyageRefreshes = Effect.gen(function* () {
 	const feeds = yield* DomainFeeds;
-	const ticks = voyageWorldTicks(feeds);
 	return <A>(read: Effect.Effect<A, SightFailure>): Stream.Stream<A, SightFailure> =>
-		Stream.unwrap(ticks.pipe(Effect.map((notices) => Stream.fromEffect(read).pipe(Stream.concat(notices.pipe(Stream.mapEffect(() => read)))))));
+		Stream.unwrap(
+			Effect.gen(function* () {
+				const voyageWrites = yield* feeds.subscribeVoyageRefresh();
+				const fleetWrites = yield* feeds.subscribeFleetRefresh();
+				const notices = Stream.merge(Stream.fromSubscription(voyageWrites), Stream.fromSubscription(fleetWrites));
+				return Stream.fromEffect(read).pipe(Stream.concat(notices.pipe(Stream.mapEffect(() => read))));
+			}),
+		);
 });

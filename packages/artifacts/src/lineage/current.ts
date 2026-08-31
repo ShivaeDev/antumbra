df0066a1c339d7c9858e6dbb72633fd4af5d6be5
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-08-31 03:55:20 +0000

    Name current artifact read effect (#378)

--- packages/artifacts/src/lineage/current.ts
+++ packages/artifacts/src/lineage/current.ts
@@ -1,11 +1,10 @@
 import { Database } from "@antumbra/persistence";
 import { Effect } from "effect";
 
-export const currentArtifactsForPiece = (pieceId: string) =>
-	Effect.gen(function* () {
-		const db = yield* Database;
-		return yield* db.Artifact.where({
-			pieceId,
-			supersededByArtifactId: null,
-		}).all();
-	});
+export const currentArtifactsForPiece = Effect.fn("artifacts.currentArtifactsForPiece")(function* (pieceId: string) {
+	const db = yield* Database;
+	return yield* db.Artifact.where({
+		pieceId,
+		supersededByArtifactId: null,
+	}).all();
+});

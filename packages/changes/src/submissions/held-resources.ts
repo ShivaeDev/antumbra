183290c1132fd4712673c48765a1ec955098e0fd
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-09-05 03:01:45 +0000

    Bound Change resource retention relation reads (#745)

--- packages/changes/src/submissions/held-resources.ts
+++ packages/changes/src/submissions/held-resources.ts
@@ -3,7 +3,7 @@ import type { HeldResource } from "@antumbra/resource-reclamation";
 import { Effect, Option } from "effect";
 import { changeRow } from "#change-read.ts";
 import { pieceChangeRow } from "#change-rows.ts";
-import { readDismissedChangeIds } from "#change-verdicts.ts";
+import { dismissedChangeIdsFor } from "#change-verdicts.ts";
 import { heldBerths } from "#held-berths.ts";
 
 const heldResource = Effect.fnUntraced(function* (resource: HeldResource) {
@@ -19,28 +19,17 @@ const heldResource = Effect.fnUntraced(function* (resource: HeldResource) {
 		}).all(),
 		changeRow,
 	);
-	const matchingLinks = yield* Effect.forEach(matchingChanges, (change) =>
-		db.PieceChange.where({ changeId: change.id })
-			.all()
-			.pipe(Effect.flatMap((rows) => Effect.forEach(rows, pieceChangeRow))),
-	);
-	const pieceIds = new Set(matchingLinks.flat().map(({ pieceId }) => pieceId));
-	const pieceLinks = yield* Effect.forEach(pieceIds, (pieceId) =>
-		db.PieceChange.where({ pieceId })
-			.all()
-			.pipe(Effect.flatMap((rows) => Effect.forEach(rows, pieceChangeRow))),
-	);
-	const links = pieceLinks.flat();
-	const changeIds = new Set([...matchingChanges.map(({ id }) => id), ...links.map(({ changeId }) => changeId)]);
-	const related = yield* Effect.forEach(changeIds, (id) => db.Change.where({ id }).first());
-	const changes = yield* Effect.forEach(
-		related.flatMap((change) => (Option.isSome(change) ? [change.value] : [])),
-		changeRow,
-	);
+	const matchingIds = new Set(matchingChanges.map(({ id }) => id));
+	const matchingLinks = yield* Effect.forEach(yield* db.PieceChange.where((link) => link.changeId.in([...matchingIds])).all(), pieceChangeRow);
+	const pieceIds = [...new Set(matchingLinks.map(({ pieceId }) => pieceId))];
+	const links = yield* Effect.forEach(yield* db.PieceChange.where((link) => link.pieceId.in(pieceIds)).all(), pieceChangeRow);
+	const relatedIds = [...new Set(links.map(({ changeId }) => changeId))].filter((id) => !matchingIds.has(id));
+	const related = yield* Effect.forEach(yield* db.Change.where((change) => change.id.in(relatedIds)).all(), changeRow);
+	const changes = [...matchingChanges, ...related];
 	return heldBerths({
 		berths: [resource],
 		changes,
-		dismissedChangeIds: yield* readDismissedChangeIds,
+		dismissedChangeIds: yield* dismissedChangeIdsFor(changes.map(({ id }) => id)),
 		pieceChanges: links,
 		repos: [repo.value],
 	});

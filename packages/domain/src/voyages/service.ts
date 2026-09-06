fa3b20a0d78c4cef2ec3e368b176846088fb57f7
Marvin Bitterlich <644950+marvin-bitterlich@users.noreply.github.com>
2026-09-06 01:26:36 +0100

    Own charter notices in VoyageProcedures

--- packages/domain/src/voyages/service.ts
+++ packages/domain/src/voyages/service.ts
@@ -6,20 +6,23 @@ import { defineService } from "@antumbra/service-definition";
 import { RoleSettings } from "@antumbra/settings";
 import { Voyages } from "@antumbra/voyages";
 import { type Context, Effect } from "effect";
+import { ExecutionSource } from "#execution/service.ts";
 import { hailCaptain } from "#hail.ts";
 import { KernelReach } from "#kernel-reach/service.ts";
 import { workPieceNow } from "#piece-work/work.ts";
 import { VoyageDetails } from "#voyage/detail/service.ts";
 import { VoyageSummaries } from "#voyage/summaries/service.ts";
 import { readVoyageView } from "#voyage-read.ts";
+import { charterWithNotice } from "#voyages/charter.ts";
 import { list } from "#voyages/list.ts";
 import { open } from "#voyages/open.ts";
 
 export const VoyageProcedureService = defineService({
 	id: "@antumbra/domain/VoyageProcedures",
-	requires: [Boards, Database, KernelReach, Pieces, RoleSettings, Rulings, VoyageDetails, VoyageSummaries, Voyages],
+	requires: [Boards, Database, ExecutionSource, KernelReach, Pieces, RoleSettings, Rulings, VoyageDetails, VoyageSummaries, Voyages],
 	initialize: Effect.void,
 	methods: () => ({
+		charterWithNotice,
 		hail: hailCaptain,
 		list,
 		open,
